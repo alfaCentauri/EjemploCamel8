da774bacc5843a0bb56c5f5edbbb39b955444294
Ricardo Presilla <ricardopresilla@gmail.com>
2023-04-04 17:43:47 -0300

    Add files

---
+++ src/main/java/com/alfaCentauri/camel/route/SimpleCamelRoute.java
@@ -0,0 +1,28 @@
+package com.alfaCentauri.camel.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleCamelRoute extends RouteBuilder {
+    @Autowired
+    Environment environment;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("{{startRoute}}")
+                .log("Timer Invoked and the body" + environment.getProperty("message"))
+                .choice()
+                .when((header("env").isNotEqualTo("mock")))
+                .pollEnrich("{{fromRoute}}")
+                .otherwise()
+                .log("mock env flow and the body is ${body}")
+                .end()
+                .to("{{toRoute1}}");
+
+
+    }
+}
